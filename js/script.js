c1282206919ddae63ed5b935a5c1bd92621ffe19
nanderson_rocha <nandersonbruno2@gmail.com>
2021-12-13 09:49:18 -0300

    animacao

---
+++ js/script.js
@@ -0,0 +1,73 @@
+// Lógica
+// 1 - Selecionar elementos que devem ser animados
+// 2 - Definir a classe que é adicionada durante a animação
+// 3 - Criar função de animação
+// 3.1 - Verificar a distância entre a barra de scroll e o topo do site
+// 3.2 - Verificar se a distância do 3.1 + Offset é maior do que a distância entre o elemento e o Topo da Página.
+// 3.3 - Se verdadeiro adicionar classe de animação, remover se for falso.
+// 4 - Ativar a função de animação toda vez que o usuário utilizar o Scroll
+// 5 - Otimizar ativação
+// Debounce do Lodash
+
+//ANIMAÇÃO ITENS SESSÕES (LÓGICA ACIMA)
+const debounce = function(func, wait, immediate) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+
+function animeScroll() {
+  const windowTop = (window.pageYOffset + (window.innerHeight * 0.75)) ;
+  target.forEach(function(element) {
+    if((windowTop) > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  })
+}
+
+animeScroll();
+
+if(target.length) {
+  window.addEventListener('scroll', debounce(function() {
+    animeScroll();
+  }, 200));
+}
+//FINAL ANIMAÇÃO ITENS SESSÕES
+
+
+//ANIMAÇÃO DIGITAÇÃO TEXTO 
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 75 * i);
+  });
+}
+
+// Se estiver tendo problemas com performance, utilize o FOR loop como abaixo no local do forEach
+// function typeWriter(elemento) {
+//   const textoArray = elemento.innerHTML.split('');
+//   elemento.innerHTML = '';
+//   for(let i = 0; i < textoArray.length; i++) {
+//     setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
+//   }
+// }
+
+const titulo = document.querySelector('h3');
+typeWriter(titulo);
+//FINAL ANIMAÇÃO DIGITAÇÃO TEXTO 
